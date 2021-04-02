347b59267d63c329cdf6b0bc27e0aa2055283052
Bryce Turley <turleyb2@gmail.com>
2021-04-01 17:56:49 -0700

    understand modules and imports

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/bturley2/meal-manager/mydb"
+	"github.com/bturley2/meal-manager/backend"
 )
 
 func main() {
 	fmt.Println("Hello World")
-	mydb.SayHello()
+	backend.SayHello()
 }
