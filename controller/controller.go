657831cd9df5c74eae7b04b29a7856a809ce6b8e
Bryce Turley <turleyb2@gmail.com>
2021-04-09 16:58:21 -0700

    reset files

--- controller/controller.go
+++
@@ -1,7 +0,0 @@
-package controller
-
-import "github.com/bturley2/meal-manager/model"
-
-type Controller struct {
-	db model.DB
-}
--- view/view.go
+++
@@ -1,9 +0,0 @@
-package view
-
-// declare interface which describes all functions available to a 'view' object
-type View interface {
-	EnterAMeal()
-	GetRecommendedMeal()
-	Run()
-	SearchByCategory()
-}
