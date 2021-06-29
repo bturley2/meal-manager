5b37a87c36f779bcf825bfe2e510068c48bfd5b6
Bryce Turley <turleyb2@gmail.com>
2021-06-29 14:37:52 -0700

    material-ui works now

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -3,39 +3,35 @@ import '../style/App.css';
 import Navbar from './Navbar';
 import MainPage from './MainPage';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
 function App() {
+  // return (
+  //   <div>
+  //     <Button variant="contained" color="primary">
+  //       Hello World
+  //     </Button>
+  //   </div>
+  // );
+
   return (
     <div>
-      <header>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-          integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" 
-          crossorigin="anonymous" >
-        </link>
-      </header>
+      <head>
+
+      </head>
 
       <body>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
         <h1>Hello World!</h1>
         <Navbar />
         <MainPage />
       </body>
     </div>
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
