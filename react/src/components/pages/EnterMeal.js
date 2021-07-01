07d3c5127f780e175cdffdcc0b6ad241a0db2318
Bryce Turley <turleyb2@gmail.com>
2021-07-01 15:24:04 -0700

    stashing changes before branch

--- react/src/components/pages/EnterMeal.js
+++ react/src/components/pages/EnterMeal.js
@@ -6,12 +6,12 @@ import Typography from '@material-ui/core/Typography';
 import useStyles from '../../style/Styles';
 
 
-export default function Dashboard() {
+export default function EnterMeal() {
   const classes = useStyles();
   return (
     <Paper className={classes.mainPage} elevation="2">
       <Typography variant="h2">
-        This is the Dashboard!
+        Enter a Meal Here!
       </Typography>
     </Paper>
   );
