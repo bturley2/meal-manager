8ad41521ab8fdf51f9fcaaaad97f69f9a61ee4b9
Bryce Turley <turleyb2@gmail.com>
2021-07-01 18:01:49 -0700

    will helped and my tabs wwork now :)

--- react/src/components/Navbar.js
+++ react/src/components/Navbar.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
-import MainPage from './MainPage';
+import MyWeekPage from "./pages/MyWeekPage";
+import UploadRecipePage from "./pages/UploadRecipePage";
 
 import AppBar from '@material-ui/core/AppBar';
 import Box from '@material-ui/core/Box';
@@ -12,6 +13,16 @@ import Typography from '@material-ui/core/Typography';
 
 
 export default function Navbar() {
+  // useStaate lets us store information in functions (it returns 2 values)
+  const [activeTab, setActiveTab] = useState(0);
+  
+  // adding an event listener to handleChange and giving it an event handler (here, changes the tabs value)
+    // event is the event that happened (onClick/onChange, etc)
+    // it holds lots of other information about the type of event
+    // event.target is the actual HTML element that triggered the event
+  const handleChange = (event, newValue) => {
+    setActiveTab(newValue);
+  };
 
   return (
     <div>
@@ -21,20 +32,25 @@ export default function Navbar() {
         <FastFoodIcon />
         <Box m={1} />
         <Typography variant="h6">Meal Manager</Typography>
-
         <Box m={2} />
 
-        <Tabs>
-          <Tab label="My Week" />
-          <Tab label="Meal Search" disabled />
-          <Tab label="Upload Recipe"  />
+        <Tabs value={activeTab} onChange={handleChange} aria-label="simple tabs example">
+          <Tab label="My Week"  />
+          <Tab label="Meal Search" />
+          <Tab label="Upload A Recipe" />
         </Tabs>
-
-
       </Toolbar>
     </AppBar>
 
-    <MainPage />
+    {activeTab === 0 && <div value={activeTab}>
+      <MyWeekPage />
+    </div>}
+    {activeTab === 1 && <div value={activeTab}>
+      <UploadRecipePage />
+    </div>}
+    {activeTab === 2 && <div value={activeTab}>
+      Item Three
+    </div>}
 
     </div>
   );
