e05f3cb8840baadfc8ab21034015a51d181a51b2
Bryce Turley <turleyb2@gmail.com>
2021-10-28 08:46:53 -0700

    got gin working

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module meal-manager
 
 go 1.16
+
+require github.com/gin-gonic/gin v1.7.4
--- server/main.go
+++ server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("hello!")
+
+	r := gin.Default()
+	r.GET("/ping", ping)
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
