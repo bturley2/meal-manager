50816becd5f90c8a7a4a374ca6e6ea8ed95dac81
Bryce Turley <turleyb2@gmail.com>
2021-12-26 18:09:31 -0800

    basic meal handling in place

--- web-server/main.go
+++ web-server/main.go
@@ -4,12 +4,20 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
+
+type MealInfo struct {
+	MealType string `json:"MealType" binding:"required"`
+	MealUrl  string `json:"MealUrl" binding:"required"`
+	Rating   int    `json:"Rating"`
+	Notes    string `json:"Notes"`
+}
 
 const (
 	dbReconnectTime = 1 * time.Second
@@ -26,8 +34,32 @@ func main() {
 	r.POST("/signup", signup)
 	r.POST("/login", login)
 
+	r.POST("/newmeal", newMealHandler)
+
 	// listen and serve on the specified port
 	r.Run(":" + os.Getenv("SERVER_LISTEN_PORT"))
+}
+
+// Adds a new meal to the database
+// This handler expects all fields to be found as JSON elements
+// in the request body
+func newMealHandler(c *gin.Context) {
+	// parse request body into
+	var newMeal MealInfo
+	if err := c.ShouldBindJSON(&newMeal); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	fmt.Println(newMeal)
+
+	// create new entry in database
+
+	c.JSON(http.StatusOK, gin.H{
+		"MealType": newMeal.MealType,
+		"Url":      newMeal.MealUrl,
+		"Rating":   newMeal.Rating,
+		"Notes":    newMeal.Notes,
+	})
 }
 
 func ping(c *gin.Context) {
